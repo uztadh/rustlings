202e99c15c7c4a6925623b2deb1087f8273b56f8
nm <nagamocha@gmail.com>
2022-12-05 00:04:01 +0300

    Complete tests exercises

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -3,12 +3,12 @@
 // pass! Make the test fail!
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        let first_val = 10;
+        let second_val = 10;
+        assert_eq!(first_val, second_val);
     }
 }
